1037f19633e99413a7c90b6781de28882bf268c1
João Sales <joao.sales@memed.com.br>
2020-03-13 18:53:40 -0300

    10281 Implements user discovery in authentication

--- src/Memed/Soluti/Auth/Session.php
+++ src/Memed/Soluti/Auth/Session.php
@@ -13,6 +13,8 @@ use Memed\Soluti\Manager;
  */
 class Session
 {
+    private const USER_STATUS_CERTIFICATES_LISTED = 'CERTIFICATES_LISTED';
+
     /**
      * @var Manager
      */
@@ -26,17 +28,98 @@ class Session
         $this->manager = $manager;
     }
 
+    private function cloudUrl(string $cloud, ?string $endpoint): string
+    {
+        return $cloud === 'birdId'
+            ? $this->manager->birdIdUrl($endpoint)
+            : $this->manager->vaultIdUrl($endpoint);
+    }
+
+    /**
+     * Creates a new application session using given credentials.
+     *
+     * @param  Credentials  $credentials
+     * @param  string  $cloud
+     * @return Token
+     */
+    private function applicationToken(Credentials $credentials, string $cloud): Token
+    {
+        $endpoint = '/oauth/client_token';
+
+        $payload = [
+            'client_id' => $credentials->client()->id($cloud),
+            'client_secret' => $credentials->client()->secret($cloud),
+            'grant_type' => 'client_credentials',
+            'lifetime' => $credentials->ttl(),
+        ];
+
+        $request = new Request(
+            'post',
+            $this->cloudUrl($cloud, $endpoint),
+            $payload
+        );
+
+        return $this->parseResponse($this->manager->client()->json($request));
+    }
+
+    /**
+     * Check which in cloud the user has certificate
+     *
+     * @param  Credentials  $credentials
+     * @return mixed
+     */
+    public function userDiscovery(Credentials $credentials)
+    {
+        $discovery = $this->userDiscoveryRequest($credentials, 'vaultId');
+
+        if ($discovery['detail']['status'] !== self::USER_STATUS_CERTIFICATES_LISTED) {
+            $discovery = $this->userDiscoveryRequest($credentials, 'birdId');
+        }
+
+        return $discovery;
+    }
+
+    /**
+     * Send request to check which in cloud the user has certificate
+     *
+     * @param  Credentials  $credentials
+     * @param  string  $cloud
+     * @return mixed
+     */
+    private function userDiscoveryRequest(Credentials $credentials, string $cloud)
+    {
+        $endpoint = "/user-discovery?document={$credentials->username()}";
+        $applicationToken = $this->applicationToken($credentials, $cloud)->token();
+
+        $request = new Request('get', $this->cloudUrl($cloud, $endpoint), [], [
+            'Authorization' => "Bearer {$applicationToken}",
+        ]);
+
+        $response = $this->manager->client()->json($request);
+
+        return array_merge([
+            'cloud' => $cloud,
+        ], json_decode((string) $response->getBody(), true));
+    }
+
     /**
      * Creates a new session using given credentials.
+     *
+     * @param  Credentials  $credentials
+     * @return Token
      */
     public function create(Credentials $credentials): Token
     {
+        $discovery = $this->userDiscovery($credentials);
+
+        $endpoint = '/oauth';
+
         $request = new Request(
             'post',
-            $this->manager->vaultIdUrl('/oauth'),
+            $this->cloudUrl($discovery['cloud'], $endpoint),
             [
-                'client_id' => $credentials->client()->id(),
-                'client_secret' => $credentials->client()->secret(),
+                'client_id' => $credentials->client()->id($discovery['cloud']),
+                'client_secret' => $credentials->client()->secret($discovery['cloud']),
                 'username' => $credentials->username(),
                 'password' => $credentials->password(),
                 'grant_type' => 'password',
